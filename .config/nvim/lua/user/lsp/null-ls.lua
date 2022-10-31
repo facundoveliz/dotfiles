4a594c403618e2f0ff8a21b3d68f1efbc763d98a
Facundo Veliz <facundoveliz9@gmail.com>
2022-10-31 18:02:54 -0300

    add prettier format

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -10,6 +10,12 @@ nls.setup({
 			filetypes = { "text", "sh", "zsh", "toml", "make", "conf", "tmux" },
 		}),
 		fmt.prettierd,
+		-- fmt.prettierd.with({
+		-- 	extra_args = {
+		-- 		"--single-quote=true",
+		-- 		"--bracket-same-line=true",
+		-- 	},
+		-- }),
 		fmt.eslint_d,
 		fmt.black,
 		fmt.stylua,
