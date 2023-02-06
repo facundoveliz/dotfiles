6d5db513c7b8e404593f19a3aaf496d263dff81c
Facundo Veliz <facundoveliz9@gmail.com>
2023-02-06 20:06:40 -0300

    updated neo-tree config

--- .config/nvim/lua/user/neo-tree.lua
+++ .config/nvim/lua/user/neo-tree.lua
@@ -1,4 +1,8 @@
 require("neo-tree").setup({
+	hide_hidden = false, -- only works on Windows for hidden files/directories
+	hide_by_name = {
+		"node_modules",
+	},
 	window = {
 		mappings = {
 			["o"] = "open",
