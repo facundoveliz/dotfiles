5099a215bc16969c1b9a4612d1af944584b301a8
Facundo Veliz <facundoveliz9@gmail.com>
2023-02-22 20:36:34 -0300

    removed innecesary stuff

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,4 +1,3 @@
--- jellybeans
 vim.g.jellybeans_overrides = {
 	SignColumn = { ctermbg = "none", guibg = "none" },
 }
@@ -12,23 +11,12 @@ if not status_ok then
 end
 
 -- remove background of floating windows
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None", fg = "None" })
--- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "None" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None", fg = "None" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "None" })
 
 -- remove background color for fold column
 vim.api.nvim_set_hl(0, "Folded", { bg = "None" })
 
--- thinner/invisible split divider (depends on theme)
-vim.api.nvim_set_hl(0, "VertSplit", { bg = "None", ctermbg = 0 })
-
 -- remove color bg from signs column
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "None", ctermbg = 0 })
 vim.api.nvim_set_hl(0, "CursorLineSign", { bg = "None", ctermbg = 0 })
-
--- remove background of pmenu
--- vim.api.nvim_set_hl(0, "Pmenu", { bg = "None", ctermbg = 0 })
--- vim.api.nvim_set_hl(0, "PmenuSbar", { bg = "None", ctermbg = 0 })
--- vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "None", ctermbg = 0 })
-
--- set the color of the vertical split bar to transparent
--- vim.api.nvim_set_hl(0, "VertSplit", { ctermbg = 0 })
