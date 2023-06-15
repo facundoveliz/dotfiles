30bdfa5a13f6e17736c272a1cd32be6657e28e21
Facundo Veliz <facundoveliz9@gmail.com>
2023-06-15 11:35:58 -0300

    code cleaning and finishing config revamp

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,15 +1,15 @@
 local M = {
-	"rose-pine/neovim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
+  "rebelot/kanagawa.nvim",
+  lazy = false,
+  priority = 1000,
 }
 
-M.name = "rose-pine"
+M.name = "kanagawa"
 function M.config()
-	local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
-	if not status_ok then
-		return
-	end
+  local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
+  if not status_ok then
+    return
+  end
 end
 
 return M
