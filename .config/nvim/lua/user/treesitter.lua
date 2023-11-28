817ccb1cb872d0742904933c547e9e16aa3249ab
Facundo Veliz <facundoveliz9@gmail.com>
2023-11-28 16:28:39 -0300

    fixed updates

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -45,11 +45,6 @@ function M.config()
 
     autotag = { enable = true },
 
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
-
     autopairs = { enable = true },
 
     textobjects = {
