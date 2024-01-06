17a78b0cdc77bc71c3acbe19448da234f0e3fafd
Facundo Veliz <facundoveliz9@gmail.com>
2024-01-06 01:47:30 -0300

    open url with gf

--- .config/nvim/lua/user/obsidian.lua
+++ .config/nvim/lua/user/obsidian.lua
@@ -1,17 +1,20 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",
-  lazy = true,
-  ft = "markdown",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "notes",
-        path = "~/Notes",
-      },
-    },
-  },
+	"epwalsh/obsidian.nvim",
+	version = "*",
+	lazy = true,
+	ft = "markdown",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "notes",
+				path = "~/Notes",
+			},
+		},
+		follow_url_func = function(url)
+			vim.fn.jobstart({ "xdg-open", url })
+		end,
+	},
 }
