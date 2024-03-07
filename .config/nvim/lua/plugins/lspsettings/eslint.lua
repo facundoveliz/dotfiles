82a9ff86f0ceebecb8b7ffd29e02f5f5f15b4c6a
Facundo Veliz <facundoveliz9@gmail.com>
2024-03-06 22:00:28 -0300

    removed unnecessary lines

--- .config/nvim/lua/plugins/lspsettings/eslint.lua
+++ .config/nvim/lua/plugins/lspsettings/eslint.lua
@@ -1,12 +1,6 @@
 return {
 	settings = {
-		enable = true,
-		format = { enable = true },
 		packageManager = "yarn",
-		workingDirectories = { mode = "auto" },
-		lintTask = {
-			enable = true,
-		},
 	},
 	on_attach = function(_, bufnr)
 		vim.api.nvim_create_autocmd("BufWritePre", {
