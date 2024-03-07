67f3c67812e9ab609a3474090cafef2d626aec16
Facundo Veliz <facundoveliz9@gmail.com>
2024-03-06 22:01:11 -0300

    code cleaning

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,21 +1,25 @@
 local M = {
-  "nvimtools/none-ls.nvim",
+	"nvimtools/none-ls.nvim",
 }
 
 function M.config()
-  local null_ls = require("null-ls")
+	local null_ls = require("null-ls")
 
-  local formatting = null_ls.builtins.formatting
+	local formatting = null_ls.builtins.formatting
 
-  null_ls.setup({
-    sources = {
-      formatting.stylua,
-      formatting.prettierd,
-      -- formatting.eslint_d,
-      formatting.black,
-      formatting.xmlformat,
-    },
-  })
+	null_ls.setup({
+		sources = {
+			formatting.stylua,
+			-- formatting.deno_fmt,
+			formatting.prettierd.with({
+				condition = function(utils)
+					return utils.has_file({ ".prettierrc", ".prettierrc.js", ".prettierrc.json" })
+				end,
+			}),
+			formatting.black,
+			formatting.xmlformat,
+		},
+	})
 end
 
 return M
