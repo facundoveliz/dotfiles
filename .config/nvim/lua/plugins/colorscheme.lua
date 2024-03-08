5ae5a27cd55e1b2d1947ef5c0c34c8df6411c165
Facundo Veliz <facundoveliz9@gmail.com>
2024-03-08 01:15:05 -0300

    replaced eslint and prettier with biome

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,37 +1,30 @@
 local M = {
-	"Mofiqul/vscode.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
+  "Mofiqul/vscode.nvim",
+  lazy = false,   -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-	local c = require("vscode.colors").get_colors()
-	require("vscode").setup({
-		-- Alternatively set style in setup
-		-- style = 'light'
+  local c = require("vscode.colors").get_colors()
+  require("vscode").setup({
+    -- Alternatively set style in setup
+    -- style = 'light'
 
-		-- Enable transparent background
-		transparent = false,
+    -- Enable transparent background
+    transparent = false,
 
-		-- Enable italic comment
-		italic_comments = true,
+    -- Enable italic comment
+    italic_comments = true,
 
-		-- Disable nvim-tree background color
-		disable_nvimtree_bg = true,
+    -- Disable nvim-tree background color
+    disable_nvimtree_bg = true,
 
-		-- Override colors (see ./lua/vscode/colors.lua)
-		color_overrides = {
-			vscLineNumber = "#FFFFFF",
-		},
-
-		-- Override highlight groups (see ./lua/vscode/theme.lua)
-		group_overrides = {
-			-- this supports the same val table as vim.api.nvim_set_hl
-			-- use colors from this colorscheme by requiring vscode.colors!
-			Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-		},
-	})
-	require("vscode").load()
+    -- Override colors (see ./lua/vscode/colors.lua)
+    color_overrides = {
+      vscLineNumber = "#FFFFFF",
+    },
+  })
+  require("vscode").load()
 end
 
 return M
