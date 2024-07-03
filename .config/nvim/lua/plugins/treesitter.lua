d6be1d124515c8cb7d748677c8c66d7dd8054435
Facundo Veliz <facundoveliz9@gmail.com>
2024-07-03 00:41:59 -0300

    updated svelte and biome config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,53 +1,44 @@
-local M = {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      event = "VeryLazy",
-    },
-    {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      event = "VeryLazy",
-    },
-    {
-      "windwp/nvim-ts-autotag",
-      event = "VeryLazy",
-    },
-    {
-      "nvim-tree/nvim-web-devicons",
-      event = "VeryLazy",
-    },
-    {
-      "windwp/nvim-autopairs",
-      event = "InsertEnter",
-    },
-  },
-}
-function M.config()
-  require("nvim-treesitter.configs").setup({
-    auto_install = true,
-    context_commentstring = {
-      config = {
-        javascript = {
-          __default = "// %s",
-          jsx_element = "{/* %s */}",
-          jsx_fragment = "{/* %s */}",
-          jsx_attribute = "// %s",
-          comment = "// %s",
-        },
-        typescript = { __default = "// %s", __multiline = "/* %s */" },
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        event = "VeryLazy",
+      },
+      {
+        "nvim-tree/nvim-web-devicons",
+        event = "VeryLazy",
+      },
+      {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true
       },
     },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },
-    autotag = { enable = true },
-    autopairs = { enable = true },
-  })
-end
-
-return M
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+        autopairs = { enable = true },
+      })
+    end,
+  },
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    event = { "BufReadPre", "BufNewFile" },
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end
+  }
+}
