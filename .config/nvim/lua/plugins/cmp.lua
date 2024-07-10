be6196430497c6b374914f4e78265bfee87a0ca8
Facundo Veliz <facundoveliz9@gmail.com>
2024-07-10 00:39:51 -0300

    even more cleaning

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,74 +1,10 @@
 return {
 	{
-		"stevearc/conform.nvim",
-		lazy = true,
-		event = { "BufReadPost", "BufNewFile" },
-		config = function()
-			local conform = require("conform")
-
-			require("conform").setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					python = { "isort", "black" },
-					javascript = { "biome" },
-					javascriptreact = { "biome" },
-					typescript = { "biome" },
-					typescriptreact = { "biome" },
-					json = { "biome" },
-				},
-				format_on_save = {
-					lsp_format = "fallback",
-					timeout_ms = 500,
-				},
-			})
-
-			vim.keymap.set({ "n", "v" }, "<leader>lf", function()
-				conform.format({
-					lsp_fallback = true,
-					async = false,
-					timeout_ms = 1000,
-				})
-			end)
-		end,
-	},
-	{
-		"mfussenegger/nvim-lint",
-		lazy = true,
-		event = { "BufReadPost", "BufNewFile" },
-		config = function()
-			local lint = require("lint")
-
-			lint.linters_by_ft = {
-				javascript = { "biomejs" },
-				typescript = { "biomejs" },
-				javascriptreact = { "biomejs" },
-				typescriptreact = { "biomejs" },
-				python = { "pylint" },
-			}
-
-			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-				group = lint_augroup,
-				callback = function()
-					lint.try_lint(nil, { ignore_errors = true })
-				end,
-			})
-
-			vim.keymap.set("n", "<leader>ll", function()
-				lint.try_lint()
-			end)
-		end,
-	},
-	{
 		"hrsh7th/nvim-cmp",
 		event = { "InsertEnter", "CmdlineEnter" },
 		dependencies = {
 			{
 				"hrsh7th/cmp-nvim-lsp",
-				event = "InsertEnter",
-			},
-			{
-				"hrsh7th/cmp-emoji",
 				event = "InsertEnter",
 			},
 			{
@@ -99,47 +35,24 @@ return {
 			},
 		},
 		config = function()
-			vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
-			vim.api.nvim_set_hl(0, "CmpItemKindTabnine", { fg = "#CA42F0" })
-			vim.api.nvim_set_hl(0, "CmpItemKindCrate", { fg = "#F64D00" })
-			vim.api.nvim_set_hl(0, "CmpItemKindEmoji", { fg = "#FDE030" })
-
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
 			require("luasnip/loaders/from_vscode").lazy_load()
 			require("luasnip").filetype_extend("typescriptreact", { "html" })
 
-			local check_backspace = function()
-				local col = vim.fn.col(".") - 1
-				return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-			end
-
-			local icons = require("config.icons")
-
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						luasnip.lsp_expand(args.body) -- For `luasnip` users.
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
 					["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-					["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-					["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-					["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-					["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 					["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-					["<C-e>"] = cmp.mapping({
-						i = cmp.mapping.abort(),
-						c = cmp.mapping.close(),
-					}),
-					-- Accept currently selected item. If none selected, `select` first item.
-					-- Set `select` to `false` to only confirm explicitly selected items.
+					["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
 					["<CR>"] = cmp.mapping.confirm({ select = false }),
 					["<Tab>"] = cmp.mapping(function(fallback)
-						-- if cmp.visible() then
-						-- 	cmp.select_next_item()
 						if luasnip.expandable() then
 							luasnip.expand()
 						elseif luasnip.expand_or_jumpable() then
@@ -149,33 +62,22 @@ return {
 						else
 							fallback()
 						end
-					end, {
-						"i",
-						"s",
-					}),
+					end, { "i", "s" }),
 					["<S-Tab>"] = cmp.mapping(function(fallback)
-						-- if cmp.visible() then
-						-- 	cmp.select_prev_item()
 						if luasnip.jumpable(-1) then
 							luasnip.jump(-1)
 						else
 							fallback()
 						end
-					end, {
-						"i",
-						"s",
-					}),
+					end, { "i", "s" }),
 				}),
 				formatting = {
 					format = function(entry, vim_item)
-						vim_item.kind = string.format("%s %s", icons.kind[vim_item.kind], vim_item.kind)
 						vim_item.menu = ({
 							nvim_lsp = "[LSP]",
 							luasnip = "[LuaSnip]",
 							buffer = "[Buffer]",
 							path = "[Path]",
-							emoji = "[Emoji]",
-							treesitter = "[Treesitter]",
 						})[entry.source.name]
 						return vim_item
 					end,
@@ -186,18 +88,63 @@ return {
 					{ name = "nvim_lua" },
 					{ name = "buffer" },
 					{ name = "path" },
-					{ name = "emoji" },
-					{ name = "treesitter" },
+				},
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		lazy = true,
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			local conform = require("conform")
+
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "biome" },
+					javascriptreact = { "biome" },
+					typescript = { "biome" },
+					typescriptreact = { "biome" },
+					svelte = { "biome" },
+					json = { "biome" },
+				},
+				format_on_save = {
+					lsp_format = "fallback",
+					timeout_ms = 500,
 				},
 			})
 
-			pcall(function()
-				local function on_confirm_done(...)
-					require("nvim-autopairs.completion.cmp").on_confirm_done()(...)
-				end
-				require("cmp").event:off("confirm_done", on_confirm_done)
-				require("cmp").event:on("confirm_done", on_confirm_done)
+			vim.keymap.set({ "n", "v" }, "<leader>lf", function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
 			end)
+		end,
+	},
+	{
+		"mfussenegger/nvim-lint",
+		lazy = true,
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+
+			lint.linters_by_ft = {
+				javascript = { "biomejs" },
+				typescript = { "biomejs" },
+				javascriptreact = { "biomejs" },
+				typescriptreact = { "biomejs" },
+			}
+
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				group = lint_augroup,
+				callback = function()
+					lint.try_lint(nil, { ignore_errors = true })
+				end,
+			})
 		end,
 	},
 }
