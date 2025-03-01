74f12cbb764ed495269daee1d171e76d12cbc08a
Facundo Veliz <fveliz@correoargentino.com.ar>
2025-03-01 13:28:18 -0300

    last

--- .screenlayout/normal.sh
+++ .screenlayout/normal.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output DP-1 --off --output DP-2 --primary --mode 2560x1440 --rate 180 --pos 1920x0 --rotate normal --output DP-3 --off --output HDMI-1 --mode 1920x1080 --rate 75 --pos 0x293 --rotate normal --output HDMI-1-2 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off
